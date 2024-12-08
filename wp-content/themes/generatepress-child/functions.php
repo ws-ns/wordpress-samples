f616010d4653909aece09cbeeb0134576c8091b7
ほわいとソフトウェア <work@wtpage.info>
2024-12-08 14:28:58 +0900

    Modify functions.php

--- wp-content/themes/generatepress-child/functions.php
+++ wp-content/themes/generatepress-child/functions.php
@@ -5,9 +5,18 @@
  */
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+  // wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  // wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
 }
+
+
+/**
+ * Remove Generatepress Styles
+ */
+add_action( 'wp_enqueue_scripts', function() {
+  wp_dequeue_style( 'generate-style' );
+  wp_dequeue_style( 'generate-child' );
+}, 999 );
 
 
 /**
